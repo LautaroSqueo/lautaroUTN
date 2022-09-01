ef7486beba8fc1126d7401ebb1cf8582eae6f3dc
LautaroSqueo <squeolautaro5@gmail.com>
2022-09-01 19:57:55 -0300

    Primer commit

---
+++ temaTresEjercicioUno/src/temaTresEjercicioUno.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : temaTresEjercicioUno.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("Hola lautaro");
+		printf("Hola lautaro");
+
+	return 0;
+}
+
+float floatNumber(float number){
+
+
+
+
+	return 0;
+}
